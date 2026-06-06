a13453f963d4d91286e865d3982060c5da2fb64e
ShrasthiSinghR <shrasthi.rathore@gmail.com>
2026-06-06 12:21:44 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (66.69%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& A) {
-        
-        int pivot=-1;
-        int n=A.size();
-        for(int i=n-2;i>=0;i--)
-        {
-            if(A[i]<A[i+1])
-            {
-                pivot=i;
-                break;
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+
+        int i = n - 2;
+
+        // Find pivot
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // If pivot exists
+        if (i >= 0) {
+            int j = n - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
             }
 
+            swap(nums[i], nums[j]);
         }
-        if(pivot==-1)
-        {
-            reverse(A.begin(),A.end());
-            return;
-        }
-        // 
-        for(int i=n-1;i>pivot;i--)
-        {
-            if(A[i]>A[pivot])
-            {
-                swap(A[i],A[pivot]);
-                break;
 
-            }
-        }
-        //other method to reverse
-        //reverse(A.begin()+pivot+1,A.end())
-
-        int i=pivot+1;
-        int j=n-1;
-        while(i<=j)
-        {
-            swap(A[i++],A[j--]);
-        }
+        // Reverse suffix
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
